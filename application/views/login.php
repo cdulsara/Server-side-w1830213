22ab7b8423a3954852d13903ab9f411c7f98672c
cdulsara <chamath.20200528@iit.ac.lk>
2024-05-11 09:59:27 +0530

    changes

--- application/views/login.php
+++ application/views/login.php
@@ -4,20 +4,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/register.css'); ?>">
     <title>Login</title>
 </head>
 <body>
+<div class="login-container">
 <div id="loginFormContainer" data-url="<?php echo base_url('auth/login_process'); ?>"></div>
 <script type="text/template" id="login-template">
-    <form id="actualLoginForm">
-        <p>Login to your account:</p>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="Enter email" value="<%= email %>" />
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" placeholder="Enter password" />
-        <button type="submit">Login</button>
-    </form>
+    <div class="login-form">
+            <div class="login-logo">
+                <div class="logo-subtext">
+                    <img class="logo" src="assets/images/logo.jpg" alt='' srcSet=''/>
+                </div>
+            </div>
+                <h2>Sign in</h2>
+                <form id="actualLoginForm">
+                    <div class="form-group">
+                        <input type="email" id="email" name="email" value="<%= email %>" />
+                        <label>Email</label>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" id="password" name="password" />
+                        <label>Password</label>
+                    </div>
+                    <div class="forgot-password">
+                        <a href="">Forgot password?</a>
+                    </div>
+                    <button type="submit">Login</button>
+                    <div class="signup">
+                        <a href ="application\views\signup.php">Not Registerd Yet? Sign Up Here</a>
+                    </div>
+                </form>
+            </div>
 </script>
     <script src="<?php echo base_url('assets/js/login.js'); ?>"></script>
+    </div>
 </body>
 </html>
--- application/views/signup.php
+++ application/views/signup.php
@@ -4,19 +4,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/register.css'); ?>">
     <title>Sign Up</title>
 </head>
 <body>
     <div id="userForm"></div>
-    <script type="text/template" id="user-template">
-        <p>Register a new user: <strong><%= username %></strong></p>
-        <form action="<?php echo base_url('auth/register'); ?>" method="post">
-        <label>Username: 
-        <input type="text" name="username" required><br>
-        <label>Email: </label><input type="email" name="email" required><br>
-        <label>Password: </label><input type="password" name="password" required><br>
-        <button type="submit">Register</button>
-        </form>
+        <script type="text/template" id="user-template">
+            <div class="login-container">
+                <div class="login-form">
+                <div class="login-logo">
+                    <div class="logo-subtext">
+                    <img class="logo" src="assets/images/logo.jpg" alt='sss' srcSet=''/>
+                    </div>
+                </div>
+                <h2>Sign up</h2>
+                <form action="<?php echo base_url('auth/register'); ?>" method="post">
+                    <div class="form-group">
+                        <input type="text" name="username" required />
+                        <label>Username</label>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="email" required />
+                        <label>Email</label>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" required />
+                        <label>Password</label>
+                    </div>
+                    <button type="submit">Sign up</button>
+                    <div class="signup">
+                        
+                    </div>
+                </form>
+            </div>
+        </div>
     </script>
 
     <script src="<?php echo base_url('assets/js/user.js'); ?>"></script>
